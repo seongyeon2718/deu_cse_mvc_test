1434e3398b553bf3083f425d7e58daff295c7e3f
Mirana02 <wjddn7491@naver.com>
2025-05-22 19:47:01 +0900

    1

---
+++ Reservation/src/main/java/com/mycompany/reservation/Reservation.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.reservation;
+
+/**
+ *
+ * @author 00rya
+ */
+public class Reservation {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
